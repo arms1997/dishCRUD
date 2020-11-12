8147cd7bf6d97615e66170cf176e9fc86eaabf6e
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2020-11-12 11:44:47 -0500

    finished mvp

--- routes/dishes.js
+++ routes/dishes.js
@@ -1,11 +1,29 @@
-const router = require('express').Router()
-const Dish = require('../models/dish')
+const router = require("express").Router();
+const Dish = require("../models/dish");
 
-router.get('/', (req, res) =>{
+router.get("/", (req, res) => {
   Dish.find()
-    .then(dishes => res.json(dishes))
-    .catch(err => res.status(400).json('Error' + err))
-})
+    .then((dishes) => res.json(dishes))
+    .catch((err) => res.status(400).json("Error" + err));
+});
 
+router.post("/", (req, res) => {
+  const dish = new Dish({
+    name: req.body.name,
+  });
 
-module.exports = router
+  dish
+    .save()
+    .then(() => res.json("New Dish Added!"))
+    .catch((err) => res.status(400).json("Error " + err));
+});
+
+router.delete("/", (req, res) => {
+  Dish.findOneAndDelete({
+    name: req.body.name,
+  })
+    .then(() => res.json("Dish with given ID has been deleted"))
+    .catch((err) => res.status(400).json("Error " + err));
+});
+
+module.exports = router;

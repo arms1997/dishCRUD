655569abe239a3050056724fa0ea99e212dd4980
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2020-11-17 10:23:47 -0500

    completed front end interface of the application

--- routes/dishes.js
+++ routes/dishes.js
@@ -17,17 +17,20 @@ router.get("/add", (req, res) => {
 });
 
 router.post("/", upload.single("dishImage"), (req, res) => {
-  let newPath = `../foodImages/${req.file.originalname}`;
+  let newPath = "";
+  if (req.file !== undefined) {
+    newPath = `../foodImages/${req.file.originalname}`;
 
-  console.log(newPath)
+    console.log(newPath);
 
-  fs.rename(req.file.path, newPath, (err) => {
-    if (err) return handleError(err, res);
-  });
+    fs.rename(req.file.path, newPath, (err) => {
+      if (err) return handleError(err, res);
+    });
+  }
 
   const dish = new Dish({
     name: req.body.dishName,
-    dishImage: newPath
+    dishImage: newPath,
   });
 
   dish
@@ -36,10 +39,17 @@ router.post("/", upload.single("dishImage"), (req, res) => {
     .catch((err) => res.status(400).json("Error " + err));
 });
 
-router.delete("/", (req, res) => {
-  Dish.findOneAndDelete({
-    name: req.body.name,
-  })
+router.get("/delete", (req, res) => {
+  Dish.find()
+    .then((dishes) => {
+      console.log(dishes);
+      res.render("pages/deleteDish", { dishes: dishes });
+    })
+    .catch((err) => res.status(400).json("Error" + err));
+});
+
+router.delete("/:id", (req, res) => {
+  Dish.findByIdAndDelete(req.params.id)
     .then(() => res.json("Dish with given ID has been deleted"))
     .catch((err) => res.status(400).json("Error " + err));
 });
